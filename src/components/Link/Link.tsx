883904521654f46561855336f3c9b96f59385f68
Samuel Pietra <samuelpietra@outlook.com>
2026-03-27 15:29:43 -0300

    fix: stops enforcing a Link prop

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -6,8 +6,10 @@ import {
 
 export type LinkProps = RadixLinkProps;
 
-export const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
-	return <RadixLink ref={ref} underline="always" {...props} />;
-});
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
+	({ underline = "always", ...props }, ref) => {
+		return <RadixLink ref={ref} underline={underline} {...props} />;
+	},
+);
 
 Link.displayName = "Link";
